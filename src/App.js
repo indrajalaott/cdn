b229e882a4c308eac19c11c5f4b750ad49bac321
dinosircode <sreejeshrayirath@gmail.com>
2024-08-19 23:10:51 +0530

    Updated Movie Image

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
-// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Cookies from 'js-cookie';
-import Login from './Pages/Login'; // Import Login
+import Login from './Pages/Login';
 import Home from './Pages/Home';
 import MovieList from './Pages/MovieList';
 import MovieDetail from './Pages/MovieDetail';
 import MovieImageComponent from './Pages/MovieImageComponent';
 
 const App = () => {
-    const token = Cookies.get('token'); // Get the token from cookies
+    const token = Cookies.get('token');
 
     return (
         <Router>
@@ -18,13 +17,16 @@ const App = () => {
                 <Route path="/" element={<Navigate to={token ? '/home' : '/login'} replace />} />
                 
                 {/* Login route */}
-                <Route path="/login" element={<Login />} /> {/* Add this line to use Login component */}
+                <Route path="/login" element={<Login />} />
                 
                 {/* Protected routes */}
                 <Route path="/home" element={token ? <Home /> : <Navigate to="/login" replace />} />
                 <Route path="/movies" element={token ? <MovieList token={token} /> : <Navigate to="/login" replace />} />
                 <Route path="/movies/:id" element={token ? <MovieDetail token={token} /> : <Navigate to="/login" replace />} />
                 <Route path="/movieImages/:imageId" element={token ? <MovieImageComponent /> : <Navigate to="/login" replace />} />
+                
+                {/* New route for serving images */}
+                <Route path="/public/movieImage/:imageId" element={<MovieImageComponent />} />
             </Routes>
         </Router>
     );
