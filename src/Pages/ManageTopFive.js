19877dfe75681c2a2484ede957b65ddbe9819017
dinosircode <sreejeshrayirath@gmail.com>
2024-10-02 12:51:01 +0530

    Uod

---
+++ src/Pages/ManageTopFive.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+
+const ManageTopFive = ({ token }) => {
+   
+
+    
+   
+
+    return (
+        <div className="find-user-container">
+            <h1>Manage Top Five Movies</h1>
+            
+            
+            
+        </div>
+    );
+};
+
+export default ManageTopFive;
